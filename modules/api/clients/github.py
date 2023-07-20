6e64768245e00911e62ac70b4f15ef4c22067fac
anya <anya.koroliova@gmail.com>
2023-07-20 19:54:08 +0300

    Test API

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -1,7 +1,11 @@
 from importlib.metadata import requires
+import re
 import requests
 
 class GitHub:
+
+    hdrs = {"Accept": "application/vnd.github+json", "Authorization": "Bearer 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"}
+
     def get_user(self, username):
         r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
@@ -15,3 +19,63 @@ class GitHub:
         body = r.json()
 
         return body
+    
+    def create_issue(self, owner, repo, data):
+        r = requests.post(f"https://api.github.com/repos/{owner}/{repo}/issues",
+                         headers=self.hdrs,
+                         json = data)
+        body = r.json()
+
+        return body
+    
+    
+    def list_of_issues(self, owner, repo, name):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/issues",
+                         headers=self.hdrs,
+                         params={"labels": name}
+                         )
+        body = r.json()
+
+        return body
+    
+    def update_issues(self, owner, repo, issue_number, data):
+        r = requests.patch(f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}",
+                        headers=self.hdrs,
+                        json = data)
+        body = r.json()
+
+        return body
+    
+    def get_issues(self, owner, repo, issue_number):
+        r = requests.patch(f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}",
+                           headers=self.hdrs)
+        body = r.json()
+
+        return body
+    
+    
+    def list_of_commit(self, owner, repo):
+        r = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits",
+                         headers=self.hdrs,
+                         params={"per_page": 1, "page" : 1}
+                         )
+        body = r.json()
+
+        return body
+    
+
+    def create_comment(self, owner, repo, commit_sha, data):
+        r = requests.post(f"https://api.github.com/repos/{owner}/{repo}/commits/{commit_sha}/comments",
+                         headers=self.hdrs,
+                         json = data)
+        body = r.json()
+
+        return body
+
+
+    def delete_comment(self, owner, repo, comment_id):
+        r = requests.delete(f"https://api.github.com/repos/{owner}/{repo}/comments/{comment_id}",
+                         headers=self.hdrs)
+        # body = r.json()
+
+        return r
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -30,3 +30,46 @@ def test_repo_cannot_be_found(github_api):
 def test_repo_with_single_char_be_found(github_api):
     r = github_api.search_repo('s')
     assert r['total_count'] != 0
+
+
+@pytest.mark.api
+def test_create_issue(github_api):
+    r1 = github_api.create_issue('AnyaKoroliova', 'qa_auto_2023', data={"title":"Found a bug","body":"I'm having a problem with this.","labels":["bug"]})
+    r2 = github_api.create_issue('AnyaKoroliova', 'qa_auto_2023', data={"title":"New bug","body":"Bug or feature.","labels":["feature"]})
+    assert 'message' not in r1
+    assert 'message' not in r2
+
+
+@pytest.mark.api
+def test_list_of_issues(github_api):
+    r = github_api.list_of_issues('AnyaKoroliova', 'qa_auto_2023', 'bug')
+    assert len(r) > 0
+    assert 'bug' in r[0]['labels'][0]['name']
+
+
+@pytest.mark.api
+def test_update_issue(github_api):
+    r = github_api.list_of_issues('AnyaKoroliova', 'qa_auto_2023', 'bug')
+    issue_id = r[0]['number']
+    ur = github_api.update_issues('AnyaKoroliova', 'qa_auto_2023', issue_id, data={"title":"Edited issue 3"})
+    i = github_api.get_issues('AnyaKoroliova', 'qa_auto_2023', issue_id)
+    assert i['title'] == "Edited issue 3"
+
+
+@pytest.mark.api
+def test_create_comment(github_api):
+    a = github_api.list_of_commit('AnyaKoroliova', 'qa_auto_2023')
+    assert 'message' not in a
+    commit_sha = a[0]['sha']
+    b = github_api.create_comment('AnyaKoroliova', 'qa_auto_2023', commit_sha, data={"body": "test comment 2"})
+    assert 'message' not in b
+    assert b['body'] == "test comment 2"
+
+
+@pytest.mark.api
+def test_delete_comment(github_api):
+    r = github_api.delete_comment('AnyaKoroliova', 'qa_auto_2023', 999)
+    body = r.json()
+    assert r.status_code == 404
+    assert 'message' in body
+    assert body['message'] == 'Not Found'
